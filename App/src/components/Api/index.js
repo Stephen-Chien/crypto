a4e81e1aeedbecb122b7d2fe8ca698e52313dc30
Stephen Chien <40301579+Stephen-Chien@users.noreply.github.com>
2021-07-13 18:54:00 -0400

    Fetched Data from API / cleaned up code

--- App/src/components/Api/index.js
+++ App/src/components/Api/index.js
@@ -1,50 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Api extends Component {
+  state = {
+    isLoading: true,
+    coin: null,
+  };
 
+  async componentDidMount() {
+    const url ="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ coin: data, isLoading: false });
+  }
 
-    state = {
-        isLoading : true,
-        coin : null
-    }
-
-
-    async componentDidMount() {
-        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
-        const response = await fetch(url)
-        const data = await response.json()
-        this.setState({coin : data[0], isLoading : false})
-        console.log(data)
-    }
-
-
-
-    render() {
-        return (
-            <div>
-                {this.state.isLoading || !this.state.coin ? <div>Loading....</div> : 
-                
-                <div>
-                    {this.state.coin.id}
-                    <br/>
-                    {this.state.coin.symbol}
-                    <br/>
-                    {this.state.coin.market_cap}
-                    <br/>
-                    {this.state.coin.image}
-                    <br/>
-                    {this.state.coin.current_price}
-                    <br/>
-                
-                </div>}
-
-
-            </div>
-
-            
-        )
-        
-    }
+  render() {
+    return (
+      <div>
+        {this.state.isLoading || !this.state.coin ? 
+        (<div>Loading....</div>) :
+        (this.state.coin.map((coin) => {
+            return (
+              <div>
+                <h1>{coin.id}</h1>
+                <h2>{coin.symbol}</h2>
+                <h3>{coin.market_cap.toLocaleString()}</h3>
+                <h4>{coin.current_price.toLocaleString()}</h4>
+              </div>
+            );
+          })
+        )}
+      </div>
+    );
+  }
 }
-
-
