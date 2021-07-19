e882245ca86a807388e164baf957b8c571cb4d0a
Stephen Chien <40301579+Stephen-Chien@users.noreply.github.com>
2021-07-19 19:45:46 -0400

    rows

---
+++ App/src/components/CoinGraph/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CoinGraph extends React.Component {
+    render() { 
+        return (  
+
+            <div>
+                <canvas id="myChart"></canvas>
+            </div>
+
+
+
+        );
+    }
+}
+ 
+export default CoinGraph;
--- App/src/index.js
+++ App/src/index.js
@@ -3,13 +3,14 @@ import Api from "./components/Api";
 import SearchBar from "./components/SearchBar";
 import React from "react";
 import ReactDOM from "react-dom";
+import CoinGraph from "./components/CoinGraph";
 
 
 
 
 ReactDOM.render(
   <div>
-    
+    <CoinGraph/>
     <Api />
   </div>
   , document.getElementById("root")
